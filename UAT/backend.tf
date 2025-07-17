437b73a7c393be251a895125c78d7ea8cc109a43
Samuel Ogah <youngtechprogram@zicloudtech.com>
2025-07-16 23:59:33 -0600

    new

--- UAT/backend.tf
+++ UAT/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "bs101-uat-app-buckettt"     #Create your s3 bucket 
-    key            = "env/uat/terraform.tfstate" # Define your desired path for the state file within the bucket
-    region         = "us-west-2"                 # Define the appropriate AWS region
-    dynamodb_table = "bs101-uat-app-dbb"          # Your DynamoDB table name for state locking
-    encrypt        = true                        # Enable encryption for the state file
-    use_lock_table = true              # instead of dynamodb_table
+    bucket         = "bs101-uat-app-buckettt"
+    key            = "uat/terraform.tfstate"         # Use a unique path per environment
+    region         = "us-west-2"
+    dynamodb_table = "bs101-uat-app-dbb"
+    encrypt        = true
   }
 }
