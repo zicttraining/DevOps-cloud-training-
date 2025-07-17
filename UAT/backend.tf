82f9907d64110dd082777912bb019fefbee3d23f
Samuel Ogah <youngtechprogram@zicloudtech.com>
2025-07-17 02:00:31 -0600

    updated

--- UAT/backend.tf
+++ UAT/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "bs101-uat-app-buckettt"
     key            = "uat/terraform.tfstate"         # Use a unique path per environment
     region         = "us-west-2"
-    dynamodb_table = "bs101-uat-app-dbb"
+    #dynamodb_table = "bs101-uat-app-dbb"
     encrypt        = true
   }
 }
