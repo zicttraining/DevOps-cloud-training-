97df7835dbf17117e0f5ec514b4550ad08118971
Samuel Ogah <youngtechprogram@zicloudtech.com>
2025-07-17 02:04:41 -0600

    updated

--- Prod/backend.tf
+++ Prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "bs101-prod-app-buckettt"
     key            = "prod/terraform.tfstate"         # Use a unique path per environment
-    region         = "us-west-2"
+    region         = "us-east-2"
     #dynamodb_table = "bs101-prod-app-dbb"
     encrypt        = true
   }
