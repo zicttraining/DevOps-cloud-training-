869f322d4d1cb7f8a64797795132f4df079c8cfb
zicttraining <118561915+zicttraining@users.noreply.github.com>
2026-08-07 00:44:00 -0600

    Update backend.tf

--- Dev/backend.tf
+++ Dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "bme-dev-app-bucket2"
-    key            = "dev/terraform.tfstate"         # Use a unique path per environment
-    region         = "us-west-2"
-    dynamodb_table = "bme-dev-app-dbb"
-    encrypt        = true
+    bucket       = "bme-dev-app-bucket2"
+    key          = "dev/terraform.tfstate"
+    region       = "us-west-2"
+    use_lockfile = true
+    encrypt      = true
   }
 }
